d0b1fb532fd656dbb1fdae42d4d3b095e6dfbc82
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2021-04-14 14:31:34 +0700

    UPDATE finish Read Diary

--- src/components/readDiary.vue
+++ src/components/readDiary.vue
@@ -1,5 +1,13 @@
 <template>
-  
+<div class="card-area">
+  <div class="text-area">
+    <h2>{{ name }}</h2>
+    <p>{{ description }}</p>
+  </div>
+  <div class="date-time">
+    <p>Written : {{date}} when {{time}}</p>
+  </div>
+</div>
 </template>
 
 <script>
@@ -9,12 +17,24 @@ export default {
     return {
     };
   },
+  props:['name','description','date','time'],
   methods: {
   },
 };
 </script>
 
 <style scoped>
-
+.card-area{
+@apply max-w-3xl py-4 px-8 bg-red-100 shadow-lg rounded-lg my-5 mx-auto;
+}
+.text-area h2 {
+  @apply text-gray-800 text-3xl font-semibold
+}
+.text-area p {
+ @apply mt-2 text-gray-600
+}
+.date-time{
+  @apply flex justify-end mt-4
+}
 
 </style>
--- src/views/DiaryDetail.vue
+++ src/views/DiaryDetail.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>This is from Diary {{ $route.params.slug }}</div>
+  <div  class="detail">
+    <go-back></go-back>
+    <read-diary :name="diary.name" :description="diary.description" :date="diary.date" :time="diary.time"></read-diary>
+  </div>
 </template>
 
 <script>
+import GoBack from '../components/GoBack.vue';
+import readDiary from '../components/readDiary';
+const axios = require("axios");
+
 export default {
-   mounted(){
-     console.log(this.slug)
+  components: { GoBack,readDiary },
+   created(){
+     this.fetchDestination();
    },
   data() {
     return {
-      
+      diary: [],
+      url: "http://localhost:5000/diaries/",
     };
   },
   props: {
@@ -18,8 +27,24 @@ export default {
       type: String,
     },
   },
+  methods:{
+    fetchDestination() {
+      axios
+        .get(this.url+this.$route.params.slug)
+        .then((response) => {
+          this.diary = response.data;
+          return response.data ;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  }
 };
 </script>
 
 <style scoped>
+.detail{
+  @apply mx-auto max-w-4xl;
+}
 </style>
